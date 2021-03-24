239aa20ad95d90effe73542837cf4c87940923e7
Cherepah <dotacontra@mailru>
2021-03-24 19:46:38 +0300

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import "./slider";
 import modals from "./modules/modals";
 import tabs from "./modules/tabs";
+import forms from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content');
     tabs('.glazing_slider', '.decoration_item', '.tabs-2', 'after_click');
+    forms();
 });
